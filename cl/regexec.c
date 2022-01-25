e921b025a54f5d220d9dc89e9dcd8def8bc2062f
Jeffrey H. Johnson <trnsz@pobox.com>
2022-01-25 06:27:04 -0500

    Fix typo

--- cl/regexec.c
+++ cl/regexec.c
@@ -54,7 +54,7 @@
 
 /* macros for manipulating states, small version */
 #define	states	long
-typedef states1	states		/* for later use in regexec() decision */
+typedef states	states1;		/* for later use in regexec() decision */
 #define	CLEAR(v)	((v) = 0)
 #define	SET0(v, n)	((v) &= ~((unsigned long)1 << (n)))
 #define	SET1(v, n)	((v) |= (unsigned long)1 << (n))
