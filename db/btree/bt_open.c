076eec5e60236af70b7b6ac84571109862513fd0
Jeffrey H. Johnson <trnsz@pobox.com>
2022-03-06 09:28:50 -0500

    Assume that AIX on POWER or PowerPC is always a big-endian system.

--- db/btree/bt_open.c
+++ db/btree/bt_open.c
@@ -69,6 +69,16 @@
 # define MINPSIZE       128
 #endif /* ifdef DEBUG */
 
+#if ( !defined(BIG_ENDIAN) && !defined(LITTLE_ENDIAN) )
+# if ( defined(_AIX) && \
+     ( defined(_POWER)  || defined(__powerpc__) || \
+       defined(__PPC__) || defined(_ARCH_PPC) ) )
+#  define BIG_ENDIAN 4321 /* Assume AIX/PPC is always big-endian */
+# endif /* if ( defined(_AIX) &&
+              ( defined(_POWER)  || defined(__powerpc__) ||
+                defined(__PPC__) || defined(_ARCH_PPC) ) ) */
+#endif /* if ( !defined(BIG_ENDIAN) && !defined(LITTLE_ENDIAN) ) */
+
 static int byteorder(void);
 static int nroot(BTREE *);
 static int tmp(void);
